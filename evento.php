ddc2956c99803b63bcc855a21305fa76a77c7f6e
Iago Sousa <iagosousa201486@gmail.com>
2023-11-03 00:11:02 -0300

    Adicionado a parte de eventos e a pagina inicial

---
+++ evento.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Eventos Bahia</title>
+  <link rel="stylesheet" href="assets/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/bootstrap.css">
+</head>
+<body>
+  <div class="container">
+    <div class="box-inicial-evento">
+      <img src="images/pato_afonso.jpg" alt="Capa Evento" class="capa-evento">
+      <h1>Trapzada</h1>
+      <p>O festival Trapzada vai rolar no dia 07 de outubro de 2023, no Wet Salvador, na Bahia. Entre as atrações confirmadas no line up, estão Filipe Ret, Cabelinho e Orochi. Os ingressos estão disponíveis.</p>
+    </div>
+    <div class="row">
+      <div class="col-lg-12 container-evento-info">
+        <div class="evento-descricao mt-5">
+          <h3>Descrição:</h3>
+          <p>Lorem ipsum dolor sit amet. Et architecto libero nam possimus magni ab distinctio omnis est repudiandae magni ut reprehenderit velit hic alias distinctio ea nemo molestias. Aut odio placeat eum dolores labore ut sint ipsum!
+
+Ut natus nesciunt est consequatur enim non sint accusantium cum sunt ipsum aut itaque iste? Non facilis officiis 33 ipsa quis ut galisum nostrum est quas magni qui voluptatem quod. In commodi laborum est internos molestiae 33 nulla dolorum.
+
+Ad sunt eveniet ad nesciunt adipisci est quod quia qui modi ipsam ut reiciendis voluptatem est blanditiis provident qui laudantium unde. Ex velit possimus At fugiat repellat est quia aspernatur ut incidunt pariatur et perspiciatis necessitatibus? Nam tempora alias id totam beatae non consequatur pariatur eum dolor quidem 33 perspiciatis eius!
+Lorem ipsum dolor sit amet. Et architecto libero nam possimus magni ab distinctio omnis est repudiandae magni ut reprehenderit velit hic alias distinctio ea nemo molestias. Aut odio placeat eum dolores labore ut sint ipsum!
+
+Ut natus nesciunt est consequatur enim non sint accusantium cum sunt ipsum aut itaque iste? Non facilis officiis 33 ipsa quis ut galisum nostrum est quas magni qui voluptatem quod. In commodi laborum est internos molestiae 33 nulla dolorum.
+
+Ad sunt eveniet ad nesciunt adipisci est quod quia qui modi ipsam ut reiciendis voluptatem est blanditiis provident qui laudantium unde. Ex velit possimus At fugiat repellat est quia aspernatur ut incidunt pariatur et perspiciatis necessitatibus? Nam tempora alias id totam beatae non consequatur pariatur eum dolor quidem 33 perspiciatis eius!</p>
+        </div>
+        <div class="evento-servico mt-5">
+          <h3>Serviço:</h3>
+          <div class="servico-info">
+            <span class="servico-pergunta">Onde:</span>
+            <span class="servico-resposta">O festival Traspanzada vai rolar no dia 07 de outubro de 2023, no Wet Salvador, na Bahia. Entre as atrações confirmadas no l</span>
+          </div>
+          <div class="servico-info">
+            <span class="servico-pergunta">Quando:</span>
+            <span class="servico-resposta">O festival Traspanzada vai rolar no dia 07 de outubro de 2023, no Wet Salvador, na Bahia. Entre as atrações confirmadas no l</span>
+          </div>
+          <div class="servico-info">
+            <span class="servico-pergunta">Atrações:</span>
+            <span class="servico-resposta">O festival Traspanzada vai rolar no dia 07 de outubro de 2023, no Wet Salvador, na Bahia. Entre as atrações confirmadas no l</span>
+          </div>
+          <div class="servico-info">
+            <span class="servico-pergunta">Classificação:</span>
+            <span class="servico-resposta"> 18 anos +</span>
+          </div>
+          <div class="servico-info">
+            <span class="servico-pergunta">Realização:</span>
+            <span class="servico-resposta">Quem irá realizar</span>
+          </div>
+          <div class="servico-info">
+            <span class="servico-pergunta">Contato:</span>
+            <span class="servico-resposta">(71) 996099120</span>
+          </div>
+        </div>
+        <!-- <div class="row">
+          <div class="col-lg-4 border mt-5"></div>
+        </div> -->
+      </div>
+    </div>
+  </div>
+  <script src="script/bootstrap.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Eventos Bahia</title>
+  <link rel="stylesheet" href="assets/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/bootstrap.css">
+</head>
+<body>
+  <div class="container">
+    <div class="box-inicial">
+      <h1>Eventos Bahia</h1>
+      <p>Segue abaixo todos os eventos na Bahia</p>
+    </div>
+    <div class="row">
+      <div class="col-lg-4 col-sm-6">
+        <div class="thumbnail">
+          <div class="img-container">
+            <img src="images/pato_afonso.jpg" alt="" class="image">
+            <div class="overlay">
+             <p class="caption"><a href="evento.php"> Saiba mais <i class="fa fa-search" aria-hidden="true"></i></a>
+            </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6">
+        <div class="thumbnail">
+          <div class="img-container">
+            <img src="images/pato_afonso.jpg" alt="" class="image">
+            <div class="overlay">
+              <p class="caption">Caption Here</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6">
+        <div class="thumbnail">
+          <div class="img-container">
+            <img src="images/pato_afonso.jpg" alt="" class="image">
+            <div class="overlay">
+              <p class="caption">Caption Here</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6">
+        <div class="thumbnail">
+          <div class="img-container">
+            <img src="images/pato_afonso.jpg" alt="" class="image">
+            <div class="overlay">
+              <p class="caption">Caption Here</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6">
+        <div class="thumbnail">
+          <div class="img-container">
+            <img src="images/pato_afonso.jpg" alt="" class="image">
+            <div class="overlay">
+              <p class="caption">Caption Here</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6">
+        <div class="thumbnail">
+          <div class="img-container">
+            <img src="images/pato_afonso.jpg" alt="" class="image">
+            <div class="overlay">
+              <p class="caption">Caption Here</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6">
+        <div class="thumbnail">
+          <div class="img-container">
+            <img src="images/pato_afonso.jpg" alt="" class="image">
+            <div class="overlay">
+              <p class="caption">Caption Here</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6">
+        <div class="thumbnail">
+          <div class="img-container">
+            <img src="images/pato_afonso.jpg" alt="" class="image">
+            <div class="overlay">
+              <p class="caption">Caption Here</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6">
+        <div class="thumbnail">
+          <div class="img-container">
+            <img src="images/pato_afonso.jpg" alt="" class="image">
+            <div class="overlay">
+              <p class="caption">Caption Here</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-sm-6">
+        <div class="thumbnail">
+          <div class="img-container">
+            <img src="images/pato_afonso.jpg" alt="" class="image">
+            <div class="overlay">
+              <p class="caption">Caption Here</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="script/bootstrap.js"></script>
+</body>
+</html>
